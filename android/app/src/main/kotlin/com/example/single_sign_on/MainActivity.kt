4657af1ebe2e4149a96e3697d579f9cd96ae5245
ravinder singh <ravi199777@gmail.com>
2024-10-14 15:48:40 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/single_sign_on/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.single_sign_on
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'AIIMS SINGLE SIGN ON',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('AIIMS SINGLE SIGN ON'),
+      ),
+      body: const Center(
+        child: Text('Hello, World!'),
+      ),
+    );
+  }
+}
